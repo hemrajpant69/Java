e9060da5e52b918bbbe40d804a3c487d8a26e363
hemrajpant69 <hemrajpant780@gmail.com>
2023-06-30 21:37:59 +0545

    JAVA

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test
+{
+    public static void main(string args[])
+    {
+        System.out.println ("Hello World!!");
+        System.out.println ("Welcome to the world of Java");
+        System.out.println ("Let us Learn Java");
+
+    }
+}
